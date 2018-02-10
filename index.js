cc245d93a82fbf858f9cde142194a0c3357f568e
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-11 00:16:16 +0100

    config des commandes

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 let Bot = require("./src/Bot.js");
 let botConfig = require("./config/config.json");
 let authConfig = require("./config/authConfig.json");
+let commands = require("./config/commands.json");
 
 
 let config = Object.assign({},botConfig,authConfig);
+config.commands = commands;
 
 let botInstance = new Bot(config);
