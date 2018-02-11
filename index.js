214233d46df55c74f5d90f5acb661a6e348db98d
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-11 02:06:31 +0100

    discord token var d'env

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 let Bot = require("./src/Bot.js");
 let botConfig = require("./config/config.json");
-let authConfig = require("./config/authConfig.json");
 let commands = require("./config/commands.json");
+
+let authConfig
+
+try {
+    authConfig = require("./config/authConfig.json");
+} catch (e) {
+    authConfig={}
+}
 
 
 let config = Object.assign({},botConfig,authConfig);
 config.commands = commands;
+
+if(!config.discord.token){
+    config.discord.token = process.env.DISCORD_TOKEN;
+}
 
 let botInstance = new Bot(config);
