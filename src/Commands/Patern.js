f304986d64d294d017eb6f74903c127e339d7905
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-13 19:47:43 +0100

    samba les couilles

--- src/Commands/Patern.js
+++ src/Commands/Patern.js
@@ -9,10 +9,15 @@ class Ping extends AbstractCommand {
         message.channel.send("https://cdn.discordapp.com/attachments/410397792167526422/412612076670812162/DFg0x2uWsAA0RNK.png").catch(console.error);
     }
 
+    runSamba(message, args){
+        message.channel.send("https://cdn.discordapp.com/attachments/351809045067202572/413042760841691156/unknown.png").catch(console.error);
+    }
+
     initialise(){
         let me = this;
         me.initialised = new Promise(function(resolve, reject) {
             me.bot.addPreg("mamba\\sles\\s?couilles",me.runMamba);
+            me.bot.addPreg("samba\\sles\\s?couilles",me.runSamba);
             resolve(me);
         });
     }
