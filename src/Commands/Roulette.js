08bb765d7192cb52eb3911349de8fa8a7bb3a257
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-13 20:07:51 +0100

    roulette

--- src/Commands/Roulette.js
+++ src/Commands/Roulette.js
@@ -1,15 +1,25 @@
-exports.run = (client, message, args) => {
+
+let AbstractCommand = require ("../AbstractCommand.js");
+
+class Roulette extends AbstractCommand {
+
+    run(message, args){
 
         message.delete(100).catch(console.error());
 
-        author = message.author.username;
+        let author = message.author.username;
 
-        message.channel.send(author + " a pressé la détente....")
+        message.channel.send(author + " a pressé la détente....");
 
-        if(Math.floor(Math.random() * 2) +1 == 1){
+        if( (Math.floor( Math.random() * (this.config.barillet || 2) ) +1) == 1){
             message.channel.send("*\CLICK\* la chambre était vide, " + author + " survit");
         }
         else{
             message.channel.send("\*PAN\* " + author + " est retrouvé(e) mort(e) dans le chat");
         }
+
     }
+
+}
+
+module.exports = Roulette;
