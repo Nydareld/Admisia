79668b36d69796c66d16e867276e5d60f5678d87
Théo Guerin <theo.guerin.pro@gmail.com>
2018-02-14 01:31:24 +0100

    Check users online

--- src/Commands/Sv.js
+++ src/Commands/Sv.js
@@ -6,12 +6,31 @@ class Sv extends AbstractCommand {
         message.delete(0).catch(console.error());
         let u1 = message.channel.members.random();
         let u2 = message.channel.members.random();
-        while (u1.user.username == u2.user.username) {
-            u2 = message.channel.members.random();
+        let x=0;
+        while (
+            ((u1.user.username == u2.user.username) ||
+            (u1.presence.status == "offline") ||
+            (u1.user.bot) ||
+            (u2.presence.status == "offline") ||
+            (u2.user.bot)) && x<100
+        ){
+            if(
+                (u1.user.username == u2.user.username)||
+                (u1.presence.status == "offline") ||
+                (u1.user.bot)
+            ){
+                u1 = message.channel.members.random();
+            }else {
+                u2 = message.channel.members.random();
+            }
+            x++;
         }
+        console.log(x);
+        console.log(u1.presence.status,u1.user.bot);
+        console.log(u2.presence.status,u2.user.bot);
         let msg = this.config.templates[Math.floor(Math.random()*this.config.templates.length)];
         let tpl = eval('`'+msg+'`');
-        message.channel.send(tpl).catch(console.error);
+        message.channel.send(tpl).then((value) => {}).catch(console.error);
     }
 }
 
