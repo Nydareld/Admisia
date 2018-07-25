6fb3398efa909eaaee215a87ade9fd78378a3dc6
Théo Guerin <theo.guerin.pro@gmail.com>
2018-07-25 14:25:42 +0200

    meilleur partern

--- src/Commands/Patern.js
+++ src/Commands/Patern.js
@@ -5,20 +5,16 @@ class Ping extends AbstractCommand {
     run(message, args){
     }
 
-    runMamba(message, args){
-        message.channel.send("https://cdn.discordapp.com/attachments/410397792167526422/412612076670812162/DFg0x2uWsAA0RNK.png").catch(console.error);
-    }
-
-    runSamba(message, args){
-        message.channel.send("https://cdn.discordapp.com/attachments/351809045067202572/413042760841691156/unknown.png").catch(console.error);
-    }
-
     initialise(){
         let me = this;
         me.initialised = new Promise(function(resolve, reject) {
-            me.bot.addPreg("mamba\\sles\\s?couilles",me.runMamba);
-            me.bot.addPreg("samba\\sles\\s?couilles",me.runSamba);
-            resolve(me);
+            for (const patern of me.config) {
+                me.bot.addPreg(patern.preg, function (message, args){
+                    message.channel
+                        .send(patern.message)
+                        .catch(console.error);
+                });
+            }
         });
     }
 
